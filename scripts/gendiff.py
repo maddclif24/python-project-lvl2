a3336a03c66d0e5e0da7e9d7c0be3a3a9d67ba15
maddclif <maddclif24@gmail.com>
2021-03-07 13:02:22 +0300

    update help

--- scripts/gendiff.py
+++ scripts/gendiff.py
@@ -5,6 +5,7 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('-f', '--format', help='set format of output')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.parse_args()
