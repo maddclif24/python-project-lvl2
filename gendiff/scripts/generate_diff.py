152513447e2c3aad5db1d7d55a6c7a686028e1b0
maddclif <maddclif24@gmail.com>
2021-03-09 18:28:04 +0300

    add yaml format

--- gendiff/scripts/generate_diff.py
+++ gendiff/scripts/generate_diff.py
@@ -2,6 +2,7 @@
 
 import argparse
 import json
+import yaml
 from gendiff.gendiff import generate_diff
 
 
@@ -11,8 +12,11 @@ def main():
     parser.add_argument('first_file', type=str)
     parser.add_argument('second_file', type=str)
     args = parser.parse_args()
-
-    file1 = json.load(open(args.first_file))
-    file2 = json.load(open(args.second_file))
-
+    if ('.json' in args.first_file):
+        file1 = json.load(open(args.first_file))
+        file2 = json.load(open(args.second_file))
+    else:
+        file1 = yaml.safe_load(open(args.first_file))
+        file2 = yaml.safe_load(open(args.second_file))
+    print(file1, file2)
     print(generate_diff(file1, file2))
--- tests/generate_diff_test.py
+++ tests/generate_diff_test.py
@@ -1,9 +1,16 @@
 from gendiff.gendiff import generate_diff
 import json
+import yaml
 
 
-file1 = json.load(open('./tests/fixtures/file_one.json'))
-file2 = json.load(open('./tests/fixtures/file_two.json'))
+flat_json_one = json.load(open('./tests/fixtures/file_one.json'))
+flat_json_two = json.load(open('./tests/fixtures/file_two.json'))
+flat_yaml_one = yaml.safe_load(open('./tests/fixtures/flat_yaml1.yml'))
+flat_yaml_two = yaml.safe_load(open('./tests/fixtures/flat_yaml2.yml'))
 
-def test_answer():
-    assert generate_diff(file1, file2) == '{\n  - follow: False\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: True\n}'
+def test_flat_json():
+    assert generate_diff(flat_json_one, flat_json_two) == '{\n  - follow: False\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: True\n}'
+
+
+def test_flat_yaml():
+    assert generate_diff(flat_yaml_one, flat_yaml_two) == '{\n  - follow: False\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: True\n}'
